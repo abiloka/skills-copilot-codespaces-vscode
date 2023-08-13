186f71993886dbd359a66f5fcd6ab97fd45a96be
abiloka <31643803+abiloka@users.noreply.github.com>
2023-08-13 19:58:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var skillsMember = document.getElementById("skillsMember");
+  var skillsMemberValue = skillsMember.value;
+  var skillsMemberText = document.getElementById("skillsMemberText");
+  skillsMemberText.innerHTML = skillsMemberValue;
+}
